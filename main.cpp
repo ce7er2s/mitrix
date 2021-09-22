bad0e502f49d541e7328238cd4ab6bb50bb620e5
ce7er2s <spike.calhun@yandex.ru>
2021-09-22 18:43:29 +0300

    Matrix class implementation pt.1

--- main.cpp
+++ main.cpp
@@ -4,10 +4,14 @@
 template <typename T> class Matrix {
 	uint32_t columns = 0;
 	uint32_t rows = 0;
-	std::vector<T, T> storage;
+public:
+	std::vector<T, T> storage = std::vector<T, T>(rows, columns);
 public:
 	void operator[] (int n) {
-		return this->storage[n];
+		if (n >= 0)
+			return this->storage[n];
+		else
+			return this->storage[this->rows-n];
 	}
 	explicit Matrix(uint32_t rows = 0, uint32_t columns = 0, std::vector<T, T>* _storage = NULL) {
 		this->columns = columns;
@@ -20,5 +24,5 @@ public:
 };
 
 int main() {
-	Matrix<uint32_t> matrix = Matrix<uint32_t>(10, 10);
+	Matrix<uint32_t> matrix = new Matrix<uint32_t>(10, 10);
 }
