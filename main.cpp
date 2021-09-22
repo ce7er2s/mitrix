22d474181c6c47e2a76d1dd180e17a0d639adfa0
ce7er2s <spike.calhun@yandex.ru>
2021-09-22 18:58:33 +0300

    Fully implemented matrix initialization

--- main.cpp
+++ main.cpp
@@ -4,25 +4,27 @@
 template <typename T> class Matrix {
 	uint32_t columns = 0;
 	uint32_t rows = 0;
+	std::vector<std::vector<T>> storage;
 public:
-	std::vector<T, T> storage = std::vector<T, T>(rows, columns);
-public:
-	void operator[] (int n) {
+	void operator[] (int32_t n) {
 		if (n >= 0)
 			return this->storage[n];
 		else
-			return this->storage[this->rows-n];
+			return this->storage[(this->rows)-n];
 	}
-	explicit Matrix(uint32_t rows = 0, uint32_t columns = 0, std::vector<T, T>* _storage = NULL) {
+	explicit Matrix(uint32_t rows = 0, uint32_t columns = 0, std::vector<std::vector<T>>* _storage = nullptr) {
 		this->columns = columns;
 		this->columns = rows;
-		if (_storage == NULL)
-			this->storage = std::vector<T, T>(rows, columns);
-		else
+		if (_storage == nullptr) {
+			this->storage = std::vector<std::vector<T>>(rows);
+			for (size_t i = 0; i < rows; i++)
+				this->storage[i].resize(this->columns);
+		} else {
 			this->storage = *_storage;
+		}
 	}
 };
 
 int main() {
-	Matrix<uint32_t> matrix = new Matrix<uint32_t>(10, 10);
+	Matrix<uint32_t> matrix = Matrix<uint32_t>(10, 10);
 }
