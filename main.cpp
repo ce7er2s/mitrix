0bbbe7a482e81234a25f25bd0a6f1ba8f16eb89e
ce7er2s <spike.calhun@yandex.ru>
2021-09-22 19:39:51 +0300

    Added inputFrom method (with custom stream attribute)

--- main.cpp
+++ main.cpp
@@ -4,30 +4,45 @@
 template <typename T> class Matrix {
 	uint32_t columns = 0;
 	uint32_t rows = 0;
+private:
 	std::vector<std::vector<T>> _storage;
 public:
-	void operator[] (int32_t n) {  // Реализация доступа по отрицательному индексу aka Python-style
+	void operator[] (int32_t n) {  // Реализация доступа к приватному _storage по отрицательному индексу aka Python-style
 		if (n >= 0)
 			return this->_storage[n];
 		else
 			return this->_storage[(this->rows)-n];
 	}
-	explicit Matrix(uint32_t rows = 0, uint32_t columns = 0, std::vector<std::vector<T>>* _storage = nullptr) {
-		this->columns = columns;
-		this->rows = rows;
+	explicit Matrix(uint32_t _rows = 0, uint32_t _columns = 0, std::vector<std::vector<T>>* _storage = nullptr) {
+		this->columns = _columns;
+		this->rows = _rows;
 		if (_storage == nullptr) {  // Запись в хранилище
-			this->_storage = std::vector<std::vector<T>>(rows);
+			this->_storage = std::vector<std::vector<T>>(rows); // иницализация масссива _storage
 			for (size_t i = 0; i < rows; i++)
 				this->_storage[i].resize(this->columns);
 		} else {
-			this->_storage = *_storage;
-		}
+			this->_storage = *_storage; // запись при наличии готового массива. Полезно при копировании матрицы.
+		}								// здесь добавить функцию get_storage c копированием массива для этого случая.
+	}
+	void inputFrom(std::istream& _stream = std::cin, uint32_t _rows = 0, uint32_t _columns = 0) {
+		if (_rows == 0)		// _stream это ссылка на поток ввода (std::ifstream легко кастуется в std::istream)
+			_rows = this->rows;
+		else
+			this->rows = _rows;
+		if (_columns == 0)
+			_columns = this->columns;
+		else
+			this->columns = _columns;
+		for (uint32_t i = 0; i < _rows; i++)
+			for (uint32_t j = 0; j < _columns; j++)
+				_stream >> this->_storage[i][j];
 	}
 };
 
 int main() {
 	auto* _storage = new std::vector<std::vector<uint32_t>>;
 	Matrix<uint32_t> matrix = Matrix<uint32_t>(2, 3, _storage);
+	matrix.inputFrom(std::cin);
 }
 
 // mitrix-cli
