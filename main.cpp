f81c9b8a0ca842f22ffc4247dcd70c1c7249e92b
ce7er2s <spike.calhun@yandex.ru>
2021-09-23 02:20:00 +0300

    overloaded two multiplying operators (* and *=). Added working determinant calculate method.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 int main() {
 	Matrix<uint32_t> matrix1 = Matrix<uint32_t>(3, 4); // Всякие проверки функциональности
 	Matrix<uint32_t> matrix2 = Matrix<uint32_t>(4, 5); // Всякие проверки функциональности
+	Matrix<uint32_t> matrix3;
 	std::ifstream ifile1 = std::ifstream("sample1.txt");
 	std::ifstream ifile2 = std::ifstream("sample2.txt");
 	matrix1.inputFrom(ifile1);
@@ -14,9 +15,8 @@ int main() {
 	matrix2.inputFrom(ifile2);
 	matrix2.printTo();
 	std::cout << std::endl;
-	matrix1.multiplyWith(matrix2);
+	matrix1 *= matrix2;
 	matrix1.printTo();
-	std::cout << matrix2.determinantOf() << std::endl;
 }
 
 // mitrix-cli
--- mitrix.cpp
+++ mitrix.cpp
@@ -13,7 +13,19 @@ public:
 	std::vector<T> operator[] (int32_t n) {
 		return this->_storage[n];	// TODO: Реализация доступа к приватному _storage по отрицательному индексу aka Python-style
 	}
-
+	Matrix<T> operator* (Matrix& _matrix_2) { // Надо проверить как я в принципе передаю параметры.
+		return Matrix<T>(*this, _matrix_2);
+	}
+	void operator*= (Matrix& _matrix) { // Плохо работает.
+		this->multiplyWith(_matrix);
+	}
+	Matrix (Matrix _matrix_1, Matrix _matrix_2) { // умножение на основе метода multiplyWith
+		this->rows = _matrix_1.rows;
+		this->columns = _matrix_2.columns;
+		this->_storage.swap(_matrix_1._storage);
+		this->multiplyWith(_matrix_2);
+	}
+	
 	explicit Matrix(uint32_t _rows = 0, uint32_t _columns = 0, std::vector<std::vector<T>>* _storage = nullptr) {
 		this->columns = _columns;
 		this->rows = _rows;
@@ -84,34 +96,36 @@ public:
 		temp.resize(temp_rows);
 		for (uint32_t i = 0; i < size; i++)
 			temp[i].resize(temp_columns);
-		for (uint32_t i = 0; i < temp_rows; i++) {
+		for (uint32_t i = 0; i < temp_rows; i++) { // Ниибаца тут тройной цикл.
 			for (uint32_t j = 0; j < temp_columns; j++) {
 				for (uint32_t k = 0; k < size; k++)
 					temp[i][j] += this->_storage[i][k] * _matrix._storage[k][j];
 			}
 		}
-		this->_storage = temp;
-		this->columns = temp_columns;
+		this->_storage.swap(temp);
+		this->columns = temp_columns; // Сам
 	}
 	T determinantOf() {
-		if (this->rows == this->columns) {
-			if (this->rows == 2) {
-				return this->_storage[0][0]*this->_storage[1][1] - this->_storage[0][1]*this->_storage[1][0];
-			} else {
+		if (this->rows == this->columns) { // Вычисляется только для кавдратной матрицы
+			if (this->rows == 2) { // Конечный случай для 2x2 матрицы
+				return (this->_storage[0][0]*this->_storage[1][1]) - (this->_storage[0][1]*this->_storage[1][0]);
+			} else if (this->rows == 1)
+				return this->_storage[0][0]; // Отдельный случай.
+			else {
 				T determinant = 0;
-				for (uint32_t i = 0; i < this->rows; i++) {
-					for (uint32_t j = 0; j < this->columns; j++) {
-						if (i+j % 2 == 0) {
-							determinant += (this->_storage[i][j] * this->submatrixOf(i,j).determinantOf());
-						} else {
-							determinant -= (this->_storage[i][j] * this->submatrixOf(i,j).determinantOf());
+				for (uint32_t j = 0; j < this->columns; j++) {
+					if (this->_storage[0][j] == 0) { // Зачем умножать, если это ноль?
+						if (j % 2 == 0) { // Миноры одной строчки.
+							determinant += this->_storage[0][j] * this->submatrixOf(0, j).determinantOf();
+						} else { // Для "четных" элементов произведение элемента и детерминанта его минора складывается и наоборот.
+							determinant -= this->_storage[0][j] * this->submatrixOf(0, j).determinantOf();
 						}
 					}
 				}
 				return determinant;
 			}
 		} else {
-			return;
+			throw std::exception(); // Я не знаю, что тут возвращать. Нужен какой-то NONE; сейчас тут throw
 		}
 	}
 };
