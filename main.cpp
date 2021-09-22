e5a995eb2ebe5442a92fbe238e3dd816cee3625d
ce7er2s <spike.calhun@yandex.ru>
2021-09-22 22:24:18 +0300

    Testing for simple in-prompt test

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
 	Matrix<uint32_t> matrix = Matrix<uint32_t>(3, 4); // Всякие проверки функциональности
 	std::ifstream ifile = std::ifstream("sample.txt");
 	matrix.inputFrom(ifile);
-	auto submatrix = matrix.submatrixOf(1, 2);
+	auto submatrix = matrix.submatrixOf(2, 1);
 	matrix.printTo();
 	std::cout << std::endl;
 	submatrix.printTo();
