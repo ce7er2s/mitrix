a2646dcdee7d6d8d269465e1003b9af6fc34e0cf
ce7er2s <spike.calhun@yandex.ru>
2021-10-01 00:07:48 +0300

    Implementation of "list" command. Print list of existing matrices.

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@
 typedef double MATRIX_T;
 
 int main() { // TODO: Вынести сервисные функции в service.cpp
+	std::ofstream output("output.txt");
 	std::vector<Matrix<MATRIX_T>> matrix_set(10);
 	for (Matrix<MATRIX_T> &matrix: matrix_set) {
 		matrix.resizeTo(10, 10);
 		matrix.fillStorage('r', 0, -10, 10);
 	}
-	Handlers::ListHandler<MATRIX_T>(matrix_set);
+	Handlers::ListHandler<MATRIX_T>(matrix_set, output);
 }
 
 // mitrix-cli
